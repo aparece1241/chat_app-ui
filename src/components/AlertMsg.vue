9c7eb1979afa2fd443bcbb86c56aaeaef3514be1
aparece1241 <apareceh7@gmail.com>
2021-04-18 22:01:23 +0800

    [UPDATE]

--- src/components/AlertMsg.vue
+++ src/components/AlertMsg.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="alert-cont">
-    <div><span>This is alert</span></div>
+    <div id="alert-msg-cont">
+      <span id="alert-icon-cont"
+        ><i class="fa fa-exclamation-circle" aria-hidden="true"></i
+      ></span>
+      <span>This is alert</span>
+    </div>
   </div>
 </template>
 
@@ -24,5 +29,26 @@ export default {
   display: grid;
   justify-items: center;
   align-items: center;
+}
+
+#alert-msg-cont {
+  border: solid green 1px;
+  padding: 20px;
+  width: 18em;
+  height: 18em;
+  background-color: rgb(242, 237, 215);
+  position: relative;
+}
+
+#alert-icon-cont {
+  border: solid 1px saddlebrown;
+  height: auto;
+  padding: 0px 0.121em;
+  font-size: 5.5em;
+  border-radius: 51px;
+  color: saddlebrown;
+  position: absolute;
+  top: -51px;
+  left: 35%;
 }
 </style>
