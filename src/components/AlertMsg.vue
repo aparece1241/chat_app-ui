fdacac064a2f02821b1df82ee122422a3a4aa982
aparece1241 <apareceh7@gmail.com>
2021-04-21 10:00:58 +0800

    [UPDATE]

--- src/components/AlertMsg.vue
+++ src/components/AlertMsg.vue
@@ -2,11 +2,15 @@
   <div id="alert-cont">
     <div class="alert-msg-cont" id="cont-wrapper">
       <div class="alert-icon-cont" id="back-border"></div>
-      <div class="alert-msg-cont">
+      <div class="alert-msg-cont" id="alert-inner-wrapper">
         <div class="alert-icon-cont">
           <img id="alert" src="@/assets/icons/success.png" />
         </div>
-        <div>This is alert</div>
+        <div id="alert-content">This is alert</div>
+        <div id="alert-btn-cont">
+          <button>ok</button>
+          <button>cancel</button>
+        </div>
       </div>
     </div>
   </div>
@@ -16,12 +20,17 @@
 export default {
   name: "Alert",
   props: {
-    message: {type: String, default: ""},
-    title: {type: String, default: ""},
-    iconName: {type: String, default: ""},
-    customStyle: {type: Object, default: null},
-    customClass: {type: Object, default: null},
-    buttons: {type: Object, default(){ return{showAll: false, ok: true, cancel: false}}}
+    message: { type: String, default: "" },
+    title: { type: String, default: "" },
+    iconName: { type: String, default: "" },
+    customStyle: { type: Object, default: null },
+    customClass: { type: Object, default: null },
+    buttons: {
+      type: Object,
+      default() {
+        return { showAll: false, ok: true, cancel: false };
+      },
+    },
   },
   data() {
     return {};
@@ -31,6 +40,9 @@ export default {
 </script>
 
 <style scoped>
+#alert-inner-wrapper {
+}
+
 .info {
   border: solid 4px #51b1de;
 }
@@ -107,6 +119,22 @@ export default {
   }
   100% {
     opacity: 1;
+  }
+}
+
+/* Media Queries */
+@media screen and (max-width: 330px) {
+  #cont-wrapper {
+    width: 305px !important;
+  }
+
+  #alert-inner-wrapper {
+    position: absolute;
+    width: 87%;
+  }
+
+  .alert-msg-cont {
+    width: 100%;
   }
 }
 </style>
